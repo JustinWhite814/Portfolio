7a356a33cc147d7aeb7401226cd8159277e2d29e
Juswhite814 <juswhite814@gmail.com>
2022-08-22 13:52:17 -0400

    more styling, rendering specific component upon click

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,7 +3,18 @@ import './navbar.scss'
 export default function navbar() {
   return (
     <div className='navbar'>
-
+      <div className="wrapper">
+        <div className="left">
+          <a href='#intro' className='logo'>genius.</a>
+          <div className="itemContainer">
+              
+          </div>
+        </div>
+        
+        <div className="right">
+          
+        </div>
+      </div>
     </div>
   )
 }
