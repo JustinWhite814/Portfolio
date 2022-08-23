d988742bbc475c1a2d72801945a6201f0f043194
Juswhite814 <juswhite814@gmail.com>
2022-08-23 19:37:49 -0400

    more changes to the intro page

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -2,10 +2,10 @@ import './intro.scss'
 
 export default function Intro() {
   return (
-    <div className='intro'id='intro'>
+    <div className='intro' id='intro'>
       <div className="left">
         <div className="imgContainer">
-          
+          <img src="assets/profilepic.png" alt="justin"/>
         </div>
       </div>
       <div className="right"></div>
