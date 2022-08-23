be3db9306cee8fcd0e8a1062a6c0f92f09254779
Juswhite814 <juswhite814@gmail.com>
2022-08-23 09:07:30 -0400

    more changes to the navbar, working on the introduction

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
+import { useState } from 'react'
 import Navbar from './components/navbar/Navbar'
 import Intro from './components/intro/Intro'
 import Portfolio from './components/portfolio/Portfolio'
 import Works from './components/works/Works'
 import Testimonials from './components/testimonials/Testimonials'
 import Contact from './components/contact/Contact'
+import Menu from './components/menu/Menu'
 import './app.scss'
 function App() {
+  const [menuOpen, setMenuOpen] = useState(false)
   return (
     <div className="app">
-      <Navbar />
+      <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
      <div className="sections">
       <Intro />
       <Portfolio />
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,18 +1,30 @@
 import './navbar.scss'
-
-export default function navbar() {
+import {Person, Mail} from '@material-ui/icons'
+export default function navbar({menuOpen, setMenuOpen}) {
   return (
-    <div className='navbar'>
+                              //if the menu is open, set the class to active
+    <div className={'navbar '+ (menuOpen && "active")}>
       <div className="wrapper">
         <div className="left">
-          <a href='#intro' className='logo'>genius.</a>
+          <a href='#intro' className='logo'>
+            genius.
+          </a>
           <div className="itemContainer">
-              
+              <Person className='icon'/>
+              <span>213 3213 2312</span>
           </div>
+       
+        <div className="itemContainer">
+            <Mail className='icon'/>
+            <span>dsaohe@gmail.com</span>
         </div>
-        
+        </div>
         <div className="right">
-          
+          <div className="hamburger" onClick={()=> setMenuOpen(!menuOpen)}>
+            <span className='line1'></span>
+            <span className='line2'></span>
+            <span className='line3'></span>
+          </div>
         </div>
       </div>
     </div>
