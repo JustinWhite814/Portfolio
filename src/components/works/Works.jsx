f62aebeedcfeb95a12568882c0959225198dd969
Juswhite814 <juswhite814@gmail.com>
2023-04-06 13:34:42 -0400

    Got the works page working

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,6 +1,8 @@
+import { useState } from 'react';
 import './works.scss'
 
 export default function Works() {
+  const [currentSlide, setCurrentSlide] = useState(0)
   const data = [
     {
       id: "1",
@@ -30,9 +32,14 @@ export default function Works() {
         "https://i.pinimg.com/originals/a9/f6/94/a9f69465d972a004ad581f245d6ad581.jpg",
     },
   ];
+  const handleClick = (direction) => {
+    direction === 'left'
+    ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 2)
+    : setCurrentSlide(currentSlide < data.length - 1 ? currentSlide + 1 : 0 )
+  }
   return (
     <div className='works' id='works'>
-     <div className="slider">
+     <div className="slider" style={{transform: `translateX(-${currentSlide * 100}vw)` }}>
       {data.map((d) =>(
       <div className="container">
        <div className="item">
@@ -55,8 +62,8 @@ export default function Works() {
        </div>
       ))}
      </div>
-     <img src="assets/arrow.png" className='arrow left' alt="" />
-     <img src="assets/arrow.png" className='arrow right' alt="" />
+     <img src="assets/arrow.png" className='arrow left' alt="" onClick={()=> handleClick('left')} />
+     <img src="assets/arrow.png" className='arrow right' alt="" onClick={()=> handleClick('right')} />
     </div>
   )
 }
