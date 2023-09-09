4a554019165cc37be191ad3eff258cfc04d94487
JustinWhite814 <juswhite814@gmail.com>
2023-09-09 10:12:14 -0400

    added some more image links"

--- src/data.js
+++ src/data.js
@@ -31,9 +31,9 @@ export const featuredPortfolio = [
   },
   {
     id: 6,
-    title: "Giphy Searcher",
+    title: "Teacher Portfolio",
     img:
-      "https://cdn.dribbble.com/users/3307260/screenshots/15468444/media/447b4501f7a145b05e11c70199a9c417.jpg?compress=1&resize=1200x900",
+      "https://i.imgur.com/hycPYsQ.png",
   },
 ];
 
@@ -41,32 +41,32 @@ export const webPortfolio = [
   {
     id: 1,
     title: "Age Calculator",
-    img: "https://cdn.dribbble.com/users/5031392/screenshots/15467520/media/c36b3b15b25b1e190d081abdbbf947cf.png?compress=1&resize=1200x900",
+    img: "https://i.imgur.com/xuFyjE9.png",
   },
   {
     id: 2,
     title: "RBG Matching Game",
-    img: "https://cdn.dribbble.com/users/3967258/screenshots/15463803/media/4fddb9a2caf3b3bd634060f706a91e73.png?compress=1&resize=1200x900",
+    img: "https://i.imgur.com/Vm2OaGk.png",
   },
   {
     id: 3,
     title: "TodoList",
-    img: "https://cdn.dribbble.com/users/3637068/screenshots/15467010/media/69a3279182b75365013fe285733d1122.png?compress=1&resize=1200x900",
+    img: "https://i.imgur.com/7jXCugK.png",
   },
   {
     id: 4,
     title: "SASS Calculator",
-    img: "https://cdn.dribbble.com/users/2264844/screenshots/15463998/media/d85e92a332c41094f68e1f716884f7ce.jpg?compress=1&resize=1200x900",
+    img: "",
   },
   {
     id: 5,
     title: "Furever Friends",
-    img: "https://cdn.dribbble.com/users/387658/screenshots/15458608/media/e92ec1dd33dd1170ae3dc9c5272f2693.jpg?compress=1&resize=1200x900",
+    img: "https://i.imgur.com/gDFixVT.png",
   },
   {
     id: 6,
     title: "Aesthetic Domain",
-    img: "https://cdn.dribbble.com/users/6384483/screenshots/15468426/media/ce9479fa1f8dba3a4a49840d76e55e31.png?compress=1&resize=1200x900",
+    img: "",
   },
 ];
 
