e7751051e76b2fdf50d3fa78560d16cfadd91387
JustinWhite814 <juswhite814@gmail.com>
2023-09-28 15:09:51 -0400

    Added in some images

--- src/data.js
+++ src/data.js
@@ -148,13 +148,13 @@ export const designPortfolio = [
     id: 5,
     title: "Design Keser Web Design",
     img:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+      "https://i.imgur.com/Nm9Z9DP.jpg",
   },
   {
     id: 6,
     title: "Design Banking App",
     img:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+      "https://i.imgur.com/Nm9Z9DP.jpg",
   },
 ];
 
@@ -163,36 +163,36 @@ export const contentPortfolio = [
     id: 1,
     title: "Content Social Media App",
     img:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+      "https://i.imgur.com/Nm9Z9DP.jpg",
   },
   {
     id: 2,
     title: "Content Rampa UI Design",
     img:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+      "https://i.imgur.com/Nm9Z9DP.jpg",
   },
   {
     id: 3,
     title: "Content E-commerce Web Design",
     img:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+      "https://i.imgur.com/Nm9Z9DP.jpg",
   },
   {
     id: 4,
     title: "Content Relax Mobile App",
     img:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+      "https://i.imgur.com/Nm9Z9DP.jpg",
   },
   {
     id: 5,
     title: "Content Keser Web Design",
     img:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+      "https://i.imgur.com/Nm9Z9DP.jpg",
   },
   {
     id: 6,
     title: "Content Banking App",
     img:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+      "https://i.imgur.com/Nm9Z9DP.jpg",
   },
 ];
